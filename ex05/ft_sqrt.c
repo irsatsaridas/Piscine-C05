f596cad985e0b5156f4eaf8412fcb45baa0ca202
irsatsaridas <109540989+irsatsaridas@users.noreply.github.com>
2022-08-09 16:46:48 +0300

    Update ft_sqrt.c

--- ex05/ft_sqrt.c
+++ ex05/ft_sqrt.c
@@ -15,15 +15,13 @@ int	ft_sqrt(int nb)
 	int	i;
 
 	i = 0;
-	if (nb > 2147395600)
-		return (0);
-	else if (nb <= 0)
+	if (nb <= 0)
 		return (0);
 	else if (nb == 1)
 		return (1);
 	else
 	{
-		while (i < nb)
+		while (i < nb && i < 46341)
 		{
 			if (i * i == nb)
 				return (i);
