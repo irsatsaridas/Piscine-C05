d82b34c763e16e44894da5f174c197779e119ca1
irsatsaridas <109540989+irsatsaridas@users.noreply.github.com>
2022-08-09 16:43:24 +0300

    Update ft_fibonacci.c

--- ex04/ft_fibonacci.c
+++ ex04/ft_fibonacci.c
@@ -12,11 +12,9 @@
 
 int	ft_fibonacci(int index)
 {
-	if (index == 0)
-		return (0);
-	else if (index == 1)
-		return (1);
-	else if (index > 1)
-		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
-	return (0);
+	if (index < 0)
+		return (-1);
+	if (index < 2)
+		return (index);
+	return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
 }
